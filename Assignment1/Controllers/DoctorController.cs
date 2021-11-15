bb6e352f56bc2e67c4a75fa6d0bee73cbf2298d2
Jens <11554129+JensBJensB@users.noreply.github.com>
2021-11-15 08:30:40 +0100

    Add project files.

---
+++ Assignment1/Controllers/DoctorController.cs
@@ -0,0 +1,34 @@
+﻿using Assignment1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Controllers
+{
+    public class DoctorController : Controller
+    {
+
+        public IActionResult FeverCheck()
+        {
+            ViewBag.Message = FeverModel.PromptForTemperature();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult FeverCheck(int temperature)
+        {
+            FeverModel fm = new FeverModel();
+            fm.Temperature = temperature;
+
+            ViewBag.Message = fm.CheckFever(temperature);
+            return View();
+        }
+    }
+}
+
+
+
+
+
---
+++ Assignment1/Models/FeverModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class FeverModel
+    {
+        public int Temperature { get; set; }
+
+        public static string PromptForTemperature() => " ";
+
+
+        public string CheckFever(int temperature)
+        {
+            string message;
+
+            if (temperature < 34)
+                message = "Tell him to set fire to his flat and get warm";
+            else if (temperature < 39)
+                message = "He is fine *hangs up*";
+            else 
+                message = "Ask if he got any last wishes";
+
+            return message;
+        }
+    }
+}
+
+
